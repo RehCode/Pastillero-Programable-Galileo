2b10ff3c89a0eeb09b7601b73c8d5aca9241cada
René Hernández <rene.rhc@gmail.com>
2017-11-18 17:34:19 -0700

    reloader en server aka cambios

--- prueba_webhilos.py
+++ prueba_webhilos.py
@@ -32,10 +32,10 @@ def hilo_sensor(sensorh, buzzer):
             buzzer.encender()
         else:
             buzzer.apagar()
-            
+
         print(sensorh.leer())
         time.sleep(0.8)
-    
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
@@ -75,7 +75,7 @@ def cont1():
 
 @route('/cont1', method='POST')
 def cont1_prog():
-    
+
     for seccion in list(cont1_secciones.keys()):
         for parametro in ['dia', 'hora', 'minuto']:
             if parametro == 'dia':
@@ -83,7 +83,7 @@ def cont1_prog():
             if parametro == 'hora':
                 cont1_secciones[seccion][parametro] = request.forms.get(seccion + 'Hora')
             if parametro == 'minuto':
-                cont1_secciones[seccion][parametro] = request.forms.get(seccion + 'Min')   
+                cont1_secciones[seccion][parametro] = request.forms.get(seccion + 'Min')
     cont1_nombre = request.forms.get('nombre')
     return template('cont1.tpl', cont1_secciones=cont1_secciones, cont1_nombre=cont1_nombre)
 
@@ -189,7 +189,7 @@ def hilo_led(led):
 def hilo_fecha(amarillo):
     global datos_leds
     nom_dia = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
-    dispensado = False                 
+    dispensado = False
     while True:
         ahora = datetime.now()
         dia = ahora.weekday()
@@ -206,7 +206,7 @@ def hilo_fecha(amarillo):
                     time.sleep(4)
                     amarillo.apagar()
                     dispensado = True
-        
+
         time.sleep(5)
         # logging.debug(datos_leds)
 
@@ -234,10 +234,10 @@ if __name__ == '__main__':
         ta.start()
         tsensor = threading.Thread(target=hilo_sensor, args=(sensorh1, buzzer))
         tsensor.setDaemon(True)
-        tsensor.start()        
+        tsensor.start()
     # python 2.7
     # t = threading.Thread(name='hilo_fecha', target=hilo)
     # t.setDaemon(True)
     # t.start()
 
-    run(host='0.0.0.0', port=8080, debug=True)
+    run(host='0.0.0.0', port=8080, debug=True, reloader=True)
